ca1ab92cc74c354bd045c5acdcf8c13f15a2b0a7
MariyaEfronY <mariyaefrony@gmail.com>
2025-10-15 11:05:22 +0530

    front page login

---
+++ app/auth/page.jsx
@@ -0,0 +1,30 @@
+    import React from 'react'
+    import Image from 'next/image'
+    import { Button } from "@/components/ui/button";
+    
+    const login = () => {
+      return (
+        <div className="flex flex-col justify-center items-center justify-center h-screen">
+          <div className="flex flex-col items-center border rounded-2xl p-8">
+            <Image src={"/logo.webp"} alt="logo" 
+            width={400}
+            height={100}
+            className='w-[180px]'
+             />      
+             <div className='flex flex-col items-center'>
+                <Image src={'/login.webp'} alt='logo'
+                width={600}
+                height={400}
+                className='w-[400px] h-[250px] rounded-2xl'
+                />
+                <h2 className="text-2xl font-bold text-center mt-5">Wellcome Back AiCruiter</h2>
+                <p className="text-gray-500 text-center">Signin With Google Authentication</p>
+                <Button className='mt-7 w-full'>Login with Google</Button>
+             </div>
+          </div>
+        </div>
+      )
+    }
+    
+    export default login
+    
--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return 
+  return ( 
+    <Button>Hello World</Button>
+  );
     
 }
