ea2ca29b8efd27a3fe66546ca6dd98e034ec117d
MariyaEfronY <mariyaefrony@gmail.com>
2025-10-15 11:57:12 +0530

    google login work perfectly

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -1,8 +1,22 @@
+    "use client"
+    
     import React from 'react'
     import Image from 'next/image'
     import { Button } from "@/components/ui/button";
+    import { supabase } from '@/services/supabaseClient';
     
-    const login = () => {
+    function Login  ()  {
+
+      const signInWithGoogle = async() => {
+        const {error}=await supabase.auth.signInWithOAuth({
+          provider:'google'
+        })
+        if(error) { 
+          console.log('Error: ',error.message)
+        }
+      }
+
+
       return (
         <div className="flex flex-col justify-center items-center justify-center h-screen">
           <div className="flex flex-col items-center border rounded-2xl p-8">
@@ -19,12 +33,13 @@
                 />
                 <h2 className="text-2xl font-bold text-center mt-5">Wellcome Back AiCruiter</h2>
                 <p className="text-gray-500 text-center">Signin With Google Authentication</p>
-                <Button className='mt-7 w-full'>Login with Google</Button>
+                <Button className='mt-7 w-full'
+                onClick={signInWithGoogle} >Login with Google</Button>
              </div>
           </div>
         </div>
       )
     }
     
-    export default login
+    export default Login
     
