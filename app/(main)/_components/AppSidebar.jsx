b4b69b60380aa1c0c0adb07822689c2daab3efac
MariyaEfronY <mariyaefrony@gmail.com>
2025-10-16 21:23:38 +0530

    sidebar logo succ inserted

--- app/(main)/_components/AppSidebar.jsx
+++ app/(main)/_components/AppSidebar.jsx
@@ -5,11 +5,20 @@ import {
   SidebarGroup,
   SidebarHeader,
 } from "@/components/ui/sidebar"
+ import Image from 'next/image'
+
+
 
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarHeader />
+      <SidebarHeader className='flex items-center' >
+      <Image alt="Logo" src="/logo.webp" 
+      width={200} 
+      height={100}
+      className="w-[150px]"
+       />
+       </SidebarHeader> 
       <SidebarContent>
         <SidebarGroup />
         <SidebarGroup />
