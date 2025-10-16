cdaa923298a0e6b397d6485a00d461903d91992f
MariyaEfronY <mariyaefrony@gmail.com>
2025-10-16 21:35:11 +0530

    ttt

--- app/(main)/_components/AppSidebar.jsx
+++ app/(main)/_components/AppSidebar.jsx
@@ -6,18 +6,21 @@ import {
   SidebarHeader,
 } from "@/components/ui/sidebar"
  import Image from 'next/image'
+ import { Button } from "@/components/ui/button"
+ import { Plus } from "lucide-react"  
 
 
 
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarHeader className='flex items-center' >
+      <SidebarHeader className='flex items-center mt-5' >
       <Image alt="Logo" src="/logo.webp" 
       width={200} 
       height={100}
       className="w-[150px]"
        />
+      <Button className='w-full mt-5'><Plus/>Add new Interview</Button>
        </SidebarHeader> 
       <SidebarContent>
         <SidebarGroup />
