4edc7890ffa640e9c83ef50cb93872b30ed90062
MariyaEfronY <mariyaefrony@gmail.com>
2025-10-16 21:15:13 +0530

    sidebare triger added

--- app/(main)/provider.js
+++ app/(main)/provider.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { SidebarProvider } from '@/components/ui/sidebar'
 import { AppSidebar } from './_components/AppSidebar'
-
+import { SidebarTrigger } from '@/components/ui/sidebar'
 
 
 const DashboardProvider = ({ children}) => {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <div>{children}</div>
+      <div>
+        <SidebarTrigger />
+        {children}
+        </div>
     </SidebarProvider>
 
 )
