1750893b26cd5d7382fa96b28357f217f5e84001
MariyaEfronY <mariyaefrony@gmail.com>
2025-10-17 18:37:30 +0530

    perfect dashboard welcome

--- app/(main)/dashboard/_components/WelcomeContainer.jsx
+++ app/(main)/dashboard/_components/WelcomeContainer.jsx
@@ -7,7 +7,7 @@ export default function WelcomeContainer() {
 
   return (
      <div>
-      <div className="bg-green-400 pt-3 rounded-2xl w-full">
+      <div className="bg-white p-5 rounded-2xl ">
       <h1 className="text-lg font-bold">Welcome Back, {user?.name || "Guest"} </h1>
       <p className="text-gray-500">Email: {user?.email || "Not available"}</p>
       <h2>AI-Driven Interview</h2>
