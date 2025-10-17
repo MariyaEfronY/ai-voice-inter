ffb4605a3cd2c7eda0a14ce85730c2d41dea4806
MariyaEfronY <mariyaefrony@gmail.com>
2025-10-17 20:20:20 +0530

    image placed dashboard

--- app/(main)/dashboard/_components/WelcomeContainer.jsx
+++ app/(main)/dashboard/_components/WelcomeContainer.jsx
@@ -7,22 +7,25 @@ export default function WelcomeContainer() {
   const { user } = useUser()
 
   return (
-     <div>
-      <div className="bg-white p-5 rounded-xl ">
-      <h1 className="text-lg font-bold">Welcome Back, {user?.name || "Guest"} </h1>
-      <p className="text-gray-500">Email: {user?.email || "Not available"}</p>
-      <h2>AI-Driven Interview</h2>
-      </div>
-             {user && (
-          <Image
-            src={user.picture}
-            alt="User Avatar"
-            width={50}
-            height={50}
-            className="rounded-full"
-          />
-        )}
+     <div className="bg-white p-5  rounded-xl flex items-center justify-between">
+  {/* Left: Text */}
+  <div>
+    <h1 className="text-lg font-bold">Welcome Back, {user?.name || "Guest"}</h1>
+    <p className="text-gray-500">Email: {user?.email || "Not available"}</p>
+    <h2 className="mt-2">AI-Driven Interview</h2>
+  </div>
 
-    </div>
+  {/* Right: Avatar */}
+  {user && (
+    <Image
+      src={user.picture || "/default-avatar.png"}
+      alt="User Avatar"
+      width={50}
+      height={50}
+      className="rounded-full"
+    />
+  )}
+</div>
+
   )
 }
