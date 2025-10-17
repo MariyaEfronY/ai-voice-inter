0325af5e493fded33212737d6e5613df2e88ee49
MariyaEfronY <mariyaefrony@gmail.com>
2025-10-17 21:13:54 +0530

    file created

---
+++ app/(main)/dashboard/_components/LatestInterviewsList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LatestInterviewsList = () => {
+    const [interviews, setInterviews] = useState([]);
+  return (
+    <div>
+      <h2 className='font-bold text-2xl' >Previosly Created Interviews</h2>
+    </div>
+  )
+}
+
+export default LatestInterviewsList
