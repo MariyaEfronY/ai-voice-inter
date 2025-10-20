e17782d274ae8e9c8cfa0fd6eeaf2fa5751a5409
MariyaEfronY <mariyaefrony@gmail.com>
2025-10-20 09:29:00 +0530

    back function working

--- app/(main)/dashboard/create-interview/page.jsx
+++ app/(main)/dashboard/create-interview/page.jsx
@@ -1,11 +1,17 @@
+
+"use client"
 import { ArrowLeft } from 'lucide-react'
+import { useRouter } from 'next/navigation'   
 import React from 'react'
 
 const CreateInterview = () => {
+  const router = useRouter();
   return (
     <div className='mt-10 px-10 md:px-24 lg:px-44 xl:px-56'>
      <div className='flex gap-5 items-center' >
-      <ArrowLeft/>
+      <ArrowLeft 
+      onClick={()=>router.back()} 
+      className='curser-pointer'/>
       <h2 className='font-bold text-2xl'>Create New Interview</h2>
      </div>
     </div>
