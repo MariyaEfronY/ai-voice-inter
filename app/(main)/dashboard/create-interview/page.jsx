946b4d80f2ffd41130c53b7a421901eaa864c2a6
MariyaEfronY <mariyaefrony@gmail.com>
2025-10-20 09:15:51 +0530

    first comit

--- app/(main)/dashboard/create-interview/page.jsx
+++ app/(main)/dashboard/create-interview/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const CreateInterview = () => {
   return (
-    <div className='p-10'>
+    <div className='mt-10 px-10 md:px-24 lg:px-44 xl:px-56'>
      <div className='flex gap-5 items-center' >
       <ArrowLeft/>
       <h2 className='font-bold text-2xl'>Create New Interview</h2>
