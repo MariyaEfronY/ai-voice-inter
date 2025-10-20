5b0c9ed2d242b711799456e7f3bfeb685410fd5e
MariyaEfronY <mariyaefrony@gmail.com>
2025-10-20 09:11:47 +0530

    create button added

--- app/(main)/dashboard/create-interview/page.jsx
+++ app/(main)/dashboard/create-interview/page.jsx
@@ -1,9 +1,13 @@
+import { ArrowLeft } from 'lucide-react'
 import React from 'react'
 
 const CreateInterview = () => {
   return (
-    <div>
-      <h1>Create Interview Page</h1>
+    <div className='p-10'>
+     <div className='flex gap-5 items-center' >
+      <ArrowLeft/>
+      <h2 className='font-bold text-2xl'>Create New Interview</h2>
+     </div>
     </div>
   )
 }
