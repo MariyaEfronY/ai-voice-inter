9a1850d28fa14aa40f36fa1097155f3de5193684
MariyaEfronY <mariyaefrony@gmail.com>
2025-10-20 09:39:38 +0530

    working dynamic progress

--- app/(main)/dashboard/create-interview/page.jsx
+++ app/(main)/dashboard/create-interview/page.jsx
@@ -3,10 +3,12 @@
 import { Progress } from '@/components/ui/progress'
 import { ArrowLeft } from 'lucide-react'
 import { useRouter } from 'next/navigation'   
+import { useState } from 'react'  
 import React from 'react'
 
 const CreateInterview = () => {
   const router = useRouter();
+  const [step, setstep] = useState(1);
   return (
     <div className='mt-10 px-10 md:px-24 lg:px-44 xl:px-56'>
      <div className='flex gap-5 items-center' >
@@ -15,7 +17,7 @@ const CreateInterview = () => {
       className='cursor-pointer'/>
       <h2 className='font-bold text-2xl'>Create New Interview</h2>
      </div>
-     <Progress value={33} className='my-5'/>
+     <Progress value={step * 33.33} className='my-5'/>
     </div>
   )
 }
