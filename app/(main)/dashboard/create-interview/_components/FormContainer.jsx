f2afea8e5451ef9cd658a9feee8348fffb005404
MariyaEfronY <mariyaefrony@gmail.com>
2025-10-21 11:49:04 +0530

    first commit

--- app/(main)/dashboard/create-interview/_components/FormContainer.jsx
+++ app/(main)/dashboard/create-interview/_components/FormContainer.jsx
@@ -1,3 +1,5 @@
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
 import React from 'react'
 
 const FormContainer = () => {
@@ -5,6 +7,12 @@ const FormContainer = () => {
     <div className='p-5 bg-white'>
       <div>
         <h2 className='text-sm'>Job Position</h2>
+        <Input placeholder="e.g. Fullstack Developer " className='mt-2' />
+      </div>
+      <div className='mt-5'>
+        <h2 className='text-sm'>Job Description</h2>
+        <Textarea placeholder="Enter details job description " 
+        className='h-[200px] mt-2' />
       </div>
     </div>
   )
