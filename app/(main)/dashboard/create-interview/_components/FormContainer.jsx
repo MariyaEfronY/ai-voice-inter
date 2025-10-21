45b558dcbd977e48704f8d37f50ed9dfdee983ea
MariyaEfronY <mariyaefrony@gmail.com>
2025-10-21 11:57:15 +0530

    secunt commit

--- app/(main)/dashboard/create-interview/_components/FormContainer.jsx
+++ app/(main)/dashboard/create-interview/_components/FormContainer.jsx
@@ -1,5 +1,12 @@
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 import React from 'react'
 
 const FormContainer = () => {
@@ -13,6 +20,21 @@ const FormContainer = () => {
         <h2 className='text-sm'>Job Description</h2>
         <Textarea placeholder="Enter details job description " 
         className='h-[200px] mt-2' />
+      </div>
+      <div className='mt-5'>
+        <h2 className='text-sm'>Interview Duration</h2>
+        <Select>
+  <SelectTrigger className="w-full mt-2">
+    <SelectValue placeholder="Select Duration" />
+  </SelectTrigger>
+  <SelectContent>
+    <SelectItem value="5 Min">5 Min</SelectItem>
+    <SelectItem value="15 Min">15 Min</SelectItem>
+    <SelectItem value="30 Min">30 Min</SelectItem>
+    <SelectItem value="45 Min">45 Min</SelectItem>
+    <SelectItem value="60 Min">60 Min</SelectItem>
+  </SelectContent>
+</Select>
       </div>
     </div>
   )
